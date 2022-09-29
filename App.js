b45464cd4e48384ee8c91081b790873e5e4a3704
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-09-29 23:59:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,9 +15,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         aman
+          Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Hello</h1>
     </div>
   );
 }
