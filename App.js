61eececff3780a55efe0651f14b101c57c5abd43
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-01 23:12:56 +0530

    AUTH (CHECK USER LOGIN)

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import "./styles.css";
+import { AuthContext } from "./Context/AuthContext";
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/Login";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>Hello</h1>
+      <Login />
+      <Dashboard />
     </div>
   );
 }
-
-export default App;
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import ThemeContextProvider from "./Context/ThemeContext";
+import AuthContextProvider from "./Context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextProvider>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </AuthContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
